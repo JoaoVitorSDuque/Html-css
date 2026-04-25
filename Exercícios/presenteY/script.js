e13bcba47264650c24f018d5aeeab091b7c362aa
joão vitor sanita duque <jvcaq.sanita@gmail.com>
2026-04-25 12:51:38 -0300

    Final

--- Exercícios/presenteY/script.js
+++ Exercícios/presenteY/script.js
@@ -2,7 +2,8 @@ var verificacao = document.getElementById('briefing')
 var verificacao2 = document.getElementById('inicio')
 var briefing = document.getElementById('Briefing')
 var secreto = document.getElementById('arestrito')
-
+var joao = document.getElementById('extra')
+var declaracao = document.getElementById('extra2')
 
 
 
@@ -14,6 +15,16 @@ function Verify() {
 
     else if(secreto.value == "yasmin") {
         alert('É com letra maiúscula o início, cabeçuda')
+    }
+
+    else if(secreto.value == "João") {
+        joao.style.display = 'block';
+        verificacao2.style.display = 'none';
+    }
+
+    else if(secreto.value == 'Extra') {
+        declaracao.style.display = 'block';
+        verificacao2.style.display = 'none';
     }
 
     else {
